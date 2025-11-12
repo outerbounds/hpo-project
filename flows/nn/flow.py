eaa2d0d1f0d5ceb4f6a91820b9bc2032bffe1349
Eddie Mattia <eddie@outerbounds.com>
2025-11-11 22:30:37 -0800

    resolve readme

--- flows/nn/flow.py
+++ flows/nn/flow.py
@@ -15,7 +15,11 @@ class NeuralNetHpoFlow(ProjectFlow):
         default="objective_fn.py",
         help="Relative path to the objective function file",
     )
+<<<<<<< HEAD
     n_trials_override = Parameter("n_trials_override", default=None, help="Total number of trials for HPO")
+=======
+    n_trials = Parameter("n_trials", default=10, help="Total number of trials for HPO")
+>>>>>>> e568fcf (resolve readme)
     trials_per_task = Parameter(
         "trials_per_task", default=1, help="Number of trials per task"
     )
@@ -35,6 +39,7 @@ class NeuralNetHpoFlow(ProjectFlow):
         elif self.config.get("direction", None) == "minimize":
             return "minimize"
         elif isinstance(self.config.get("directions", None), list):
+<<<<<<< HEAD
             assert (
                 len(self.config.get("directions", None)) == 2
             ), "Direction of multi-objective optimization direction list must be a list of two values."
@@ -49,13 +54,30 @@ class NeuralNetHpoFlow(ProjectFlow):
         python=config.environment.get("python"),
         packages=config.environment.get("packages"),
     )
+=======
+            assert len(self.config.get("directions", None)) == 2, "Direction of multi-objective optimization direction list must be a list of two values."
+            return self.config.get("directions")
+        else:
+            docs_ref = "https://optuna.readthedocs.io/en/stable/reference/generated/optuna.study.StudyDirection.html#optuna.study.StudyDirection"
+            raise ValueError(f"Invalid direction: {self.config.get('direction', None)}. See {docs_ref} for more information.")
+
+    @pypi(python=config.environment.get("python"), packages=config.environment.get("packages"))
+>>>>>>> e568fcf (resolve readme)
     @step
     def start(self):
         import optuna
         from utils import cache_mnist
 
+<<<<<<< HEAD
         self.n_trials = self.n_trials_override or self.config.get("n_trials", 10)
         self.workers = list(range(min(self.n_trials, self.config.get("max_parallelism", 10))))
+=======
+        num_full_batches = self.n_trials // self.trials_per_task
+        remaining_trials = self.n_trials % self.trials_per_task        
+        self.batches = [self.trials_per_task] * num_full_batches
+        if remaining_trials > 0:
+            self.batches.append(remaining_trials)
+>>>>>>> e568fcf (resolve readme)
         
         override_study_name = (
             None
@@ -70,8 +92,12 @@ class NeuralNetHpoFlow(ProjectFlow):
         print(f"Study name: {self.study_name}")
 
         ### Cache Dataset
+<<<<<<< HEAD
         # cache_mnist()
         # NOTE: Turned off for now as each worker downloads the small DS, see utils.py for details.
+=======
+        cache_mnist()
+>>>>>>> e568fcf (resolve readme)
 
         ### Configure Study
         self.study_kwargs = dict(
@@ -88,6 +114,7 @@ class NeuralNetHpoFlow(ProjectFlow):
         else:
             self.study_kwargs["direction"] = diresult
         _ = optuna.create_study(**self.study_kwargs)
+<<<<<<< HEAD
         self.next(self.run_trial, foreach="workers")
 
     @kubernetes(compute_pool=config.compute_pool)
@@ -95,12 +122,19 @@ class NeuralNetHpoFlow(ProjectFlow):
         python=config.environment.get("python"),
         packages=config.environment.get("packages"),
     )
+=======
+        self.next(self.run_trial, foreach="batches")
+
+    @kubernetes(compute_pool=config.compute_pool)
+    @pypi(python=config.environment.get("python"), packages=config.environment.get("packages"))
+>>>>>>> e568fcf (resolve readme)
     @step
     def run_trial(self):
         import optuna
         from utils import load_objective_function
 
         self.objective = load_objective_function(self.objective_function_file)
+<<<<<<< HEAD
 
         # Each task runs exactly 1 trial for 1:1 task-trial mapping
         study = optuna.create_study(**self.study_kwargs)
@@ -120,14 +154,26 @@ class NeuralNetHpoFlow(ProjectFlow):
     @pypi(python=config.environment.get("python"), packages=config.environment.get("packages"))
     @step
     def post_trial(self):
+=======
+        # Each task runs a batch of trials, may be just 1 for cleanest (trial, model, task) accounting.
+        # TODO: Refactor as conditionals/looping DAGs lands. Revisit this.
+        batch_size = self.input
+        study = optuna.create_study(**self.study_kwargs)
+        study.optimize(self.objective, n_trials=batch_size)
+        self.trial_result = {"batch_size": batch_size, "completed": True}
+>>>>>>> e568fcf (resolve readme)
         self.next(self.join)
 
     @card(id="best_model")
     @kubernetes(compute_pool=config.compute_pool)
+<<<<<<< HEAD
     @pypi(
         python=config.environment.get("python"),
         packages=config.environment.get("packages"),
     )
+=======
+    @pypi(python=config.environment.get("python"), packages=config.environment.get("packages"))
+>>>>>>> e568fcf (resolve readme)
     @step
     def join(self, inputs):
         from utils import load_study
@@ -144,10 +190,14 @@ class NeuralNetHpoFlow(ProjectFlow):
         )
         self.next(self.end)
 
+<<<<<<< HEAD
     @pypi(
         python=config.environment.get("python"),
         packages=config.environment.get("packages"),
     )
+=======
+    @pypi(python=config.environment.get("python"), packages=config.environment.get("packages"))
+>>>>>>> e568fcf (resolve readme)
     @step
     def end(self):
         print("Flow completed")
